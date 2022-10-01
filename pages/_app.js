de6f093fc1417c5abbdf5668fe88ea16bb01fe4c
Hamed <1385656+Hameds@users.noreply.github.com>
2022-10-01 18:56:42 +0330

    remove fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,33 +1,14 @@
 import * as React from 'react'
 import App from 'next/app';
-import Fathom from 'fathom-client'
-import Router from 'next/router'
 import Providers from '../components/Providers';
-
-Router.events.on('routeChangeComplete', () => {
-  Fathom.trackPageview()
-})
-
-function FathomWrapper(props) {
-  React.useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      Fathom.load();
-      Fathom.setSiteId('ESMHTGZE');
-      Fathom.trackPageview();
-    }
-  }, [])
-  return <div {...props} />
-}
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <FathomWrapper>
         <Providers>
           <Component {...pageProps} />
         </Providers>
-      </FathomWrapper>
     );
   }
 }
