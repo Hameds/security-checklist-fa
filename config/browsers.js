5198eefb516fd06eb95d8ffd4690f15d4eef004b
Hamed <1385656+Hameds@users.noreply.github.com>
2023-02-01 12:54:23 +0330

    resolve #3 remove Brave and add Librewolf & DuckDuckGo
    Signed-off-by: Hamed <1385656+Hameds@users.noreply.github.com>

--- config/browsers.js
+++ config/browsers.js
@@ -7,21 +7,6 @@ export default {
     `,
   apps: [
     {
-      name: "Brave",
-      image: "/static/img/brave.jpg",
-      url: "https://brave.com/",
-      sources: {
-        windows: "https://laptop-updates.brave.com/latest/winx64",
-        macos: "https://laptop-updates.brave.com/latest/osx",
-        ios:
-          "https://geo.itunes.apple.com/us/app/brave-web-browser/id1052879175?mt=8",
-        android:
-          "https://play.google.com/store/apps/details?id=com.brave.browser&hl=en",
-        linux:
-          "https://brave-browser.readthedocs.io/en/latest/installing-brave.html#linux"
-      }
-    },
-    {
       name: "Firefox",
       image: "/static/img/firefox.jpg",
       url: "https://www.mozilla.org/en-US/firefox/",
@@ -30,17 +15,8 @@ export default {
         macos: "https://www.mozilla.org/en-US/firefox/",
         ios: "https://www.mozilla.org/en-US/firefox/mobile/",
         android: "https://www.mozilla.org/en-US/firefox/mobile/",
-        linux: "https://support.mozilla.org/en-US/kb/install-firefox-linux"
-      }
-    },
-    {
-      name: "Safari",
-      image: "/static/img/safari.jpg",
-      url: "https://www.apple.com/safari/",
-      sources: {
-        macos: "https://www.apple.com/safari/",
-        ios: "https://www.apple.com/safari/"
-      }
+        linux: "https://support.mozilla.org/en-US/kb/install-firefox-linux",
+      },
     },
     {
       name: "Tor",
@@ -52,69 +28,67 @@ export default {
         macos: "https://www.torproject.org/download/download-easy.html#mac",
         linux: "https://www.torproject.org/download/download-easy.html#linux",
         android:
-          "https://play.google.com/store/apps/details?id=org.torproject.torbrowser_alpha"
-      }
+          "https://play.google.com/store/apps/details?id=org.torproject.torbrowser_alpha",
+      },
     },
     {
-      name: "Vivaldi",
-      image: "/static/img/vivaldi.png",
-      url: "https://www.vivaldi.com/",
+      name: "Safari",
+      image: "/static/img/safari.jpg",
+      url: "https://www.apple.com/safari/",
       sources: {
-        windows: "https://vivaldi.com/download/",
-        macos: "https://vivaldi.com/download/",
-        linux: "https://vivaldi.com/download/",
-        android: "https://vivaldi.com/android/"
-      }
+        macos: "https://www.apple.com/safari/",
+        ios: "https://www.apple.com/safari/",
+      },
     },
     {
-      name: "Cliqz",
-      image: "/static/img/cliqz.png",
-      url: "https://cliqz.com/",
+      name: "Librewolf",
+      image: "/static/img/librewolf.png",
+      url: "https://librewolf.net/",
       sources: {
-        windows: "https://cliqz.com/download",
-        macos: "https://cliqz.com/download",
-        ios:
-          "https://itunes.apple.com/de/app/cliqz-browser-suchmaschine/id1065837334?mt=8",
+        windows: "https://librewolf.net/installation/windows/",
+        macos: "https://librewolf.net/installation/macos/",
+        linux: "https://librewolf.net/installation/debian/",
+      },
+    },
+    {
+      name: "DuckDuckGo",
+      image: "/static/img/duckduckgo.jpg",
+      url: "https://duckduckgo.com/app",
+      sources: {
+        ios: "https://apps.apple.com/app/duckduckgo-privacy-browser/id663592361",
         android:
-          "https://play.google.com/store/apps/details?hl=de&id=com.cliqz.browser"
-      }
-    }
+          "https://play.google.com/store/apps/details?id=com.duckduckgo.mobile.android",
+      },
+    },
   ],
   resources: [
     {
       name: "Don’t expect privacy from Chrome",
-      url:
-        "https://www.digitaltrends.com/computing/google-chrome-incognito-mode-tracking/"
+      url: "https://www.digitaltrends.com/computing/google-chrome-incognito-mode-tracking/",
     },
     {
       name: "Google Chrome‘s users take a back seat to its bottom line",
-      url:
-        "https://www.eff.org/deeplinks/2018/11/google-chromes-users-take-back-seat-its-bottom-line"
+      url: "https://www.eff.org/deeplinks/2018/11/google-chromes-users-take-back-seat-its-bottom-line",
     },
     {
       name: "What data of mine does Chrome send to Google?",
-      url:
-        "https://lifehacker.com/5763452/what-data-does-chrome-send-to-google-about-me"
+      url: "https://lifehacker.com/5763452/what-data-does-chrome-send-to-google-about-me",
     },
     {
       name: "Firefox multi-account containers",
-      url:
-        "https://addons.mozilla.org/en-US/firefox/addon/multi-account-containers/"
+      url: "https://addons.mozilla.org/en-US/firefox/addon/multi-account-containers/",
     },
     {
       name: "How to protect yourself from browser fingerprinting",
-      url:
-        "https://www.comparitech.com/blog/vpn-privacy/what-is-browser-fingerprinting-how-to-protect-yourself/"
+      url: "https://www.comparitech.com/blog/vpn-privacy/what-is-browser-fingerprinting-how-to-protect-yourself/",
     },
     {
       name: "Browser fingerprinting, and why they are so hard to erase",
-      url:
-        "https://www.networkworld.com/article/2884026/security0/browser-fingerprints-and-why-they-are-so-hard-to-erase.html"
+      url: "https://www.networkworld.com/article/2884026/security0/browser-fingerprints-and-why-they-are-so-hard-to-erase.html",
     },
     {
-      name:
-        "Who Tracks Me - Learn about tracking technologies, market structure and data-sharing on the web.",
-      url: "https://whotracks.me/"
-    }
-  ]
+      name: "Who Tracks Me - Learn about tracking technologies, market structure and data-sharing on the web.",
+      url: "https://whotracks.me/",
+    },
+  ],
 };
