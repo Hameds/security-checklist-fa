f20bb3686ad1bd2615d4ea4ddc95218733d51d31
Hamed <1385656+Hameds@users.noreply.github.com>
2023-02-17 12:12:40 +0330

    update geotagging resources
    Signed-off-by: Hamed <1385656+Hameds@users.noreply.github.com>

--- config/geotagging.js
+++ config/geotagging.js
@@ -11,41 +11,13 @@ export default {
   `,
   resources: [
     {
-      name: "Are Location-Tagged Photos Really a Privacy Concern?",
+      name: "حذف اطلاعات EXIF در سیستم عامل‌های مختلف",
       url:
-        "https://www.howtogeek.com/340566/are-location-tagged-photos-really-a-privacy-concern/"
-    },
-    {
-      name:
-        "Software Roundup: 5 of The Best Apps to Remove EXIF Data from Images",
-      url:
-        "https://www.maketecheasier.com/best-apps-remove-exif-data-from-images/"
-    },
+        "https://hamed.blog/exif/"
+    },    
     {
       name: "Geotagging",
       url: "https://en.wikipedia.org/wiki/Geotagging"
-    },
-    {
-      name: "What is geotag security and how it helps to protect your family",
-      url: "https://www.geotag-security.com/"
-    },
-    {
-      name: "Web photos that reveal secrets, like where you live",
-      url:
-        "https://www.nytimes.com/2010/08/12/technology/personaltech/12basics.html"
-    },
-    {
-      name: "How to avoid the potential risks of geotagging",
-      url: "https://www.wikihow.com/Avoid-the-Potential-Risks-of-Geotagging"
-    },
-    {
-      name: "How to turn off geotagging for photos on iPhone and iPad",
-      url:
-        "https://www.techbout.com/turn-off-geotagging-for-photos-iphone-ipad-8738/"
-    },
-    {
-      name: "How To turn off geotagging on Android devices",
-      url: "https://www.technobezz.com/turn-off-geotagging-android-devices/"
-    }
+    }    
   ]
 };
