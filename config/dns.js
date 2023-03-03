9e2be2e43bdceba5f8fa38f4620607e9e645f2c9
Hamed <1385656+Hameds@users.noreply.github.com>
2023-03-03 10:51:31 +0330

    update dns resources
    Signed-off-by: Hamed <1385656+Hameds@users.noreply.github.com>

--- config/dns.js
+++ config/dns.js
@@ -36,34 +36,8 @@ export default {
   ],
   resources: [
     {
-      name: "What is 1.1.1.1?",
+      name: "1.1.1.1 چیست؟",
       url: "https://www.cloudflare.com/learning/dns/what-is-1.1.1.1/"
-    },
-    {
-      name:
-        "Cloudflare launches 1.1.1.1 DNS service that will speed up your internet",
-      url:
-        "https://www.theverge.com/2018/4/1/17185732/cloudflare-dns-service-1-1-1-1"
-    },
-    {
-      name: "Cloudflare’s privacy-focused 1.1.1.1 is available on phones",
-      url:
-        "https://www.engadget.com/2018/11/11/cloudflare-1-1-1-1-privacy-service-on-phones/"
-    },
-    {
-      name: "New “Quad9” DNS service blocks malicious domains for everyone",
-      url:
-        "https://arstechnica.com/information-technology/2017/11/new-quad9-dns-service-blocks-malicious-domains-for-everyone/"
-    },
-    {
-      name: "Quad9, a Public DNS Resolver - with Security",
-      url:
-        "https://labs.ripe.net/Members/stephane_bortzmeyer/quad9-a-public-dns-resolver-with-security/"
-    },
-    {
-      name: "Cloudflare and Quad9 Aim to Improve DNS",
-      url:
-        "https://tidbits.com/2018/04/20/cloudflare-and-quad9-aim-to-improve-dns/"
-    }
+    }    
   ]
 };
