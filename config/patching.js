c328a2c73d93d4b8fdd67b3dbeff06f31d441875
Hamed <1385656+Hameds@users.noreply.github.com>
2023-03-03 10:53:27 +0330

    update patching resources
    Signed-off-by: Hamed <1385656+Hameds@users.noreply.github.com>

--- config/patching.js
+++ config/patching.js
@@ -14,29 +14,8 @@ export default {
         "https://www.zoomit.ir/2019/6/4/336637/should-not-stop-windows-10-update/"
     },
     {
-      name: "Stop putting off your device updates—here's why",
-      url: "https://www.popsci.com/update-every-gadget"
-    },
-    {
-      name: "Rarely Patched Software Bugs in Home Routers Cripple Security",
-      url:
-        "https://www.wsj.com/articles/rarely-patched-software-bugs-in-home-routers-cripple-security-1453136285"
-    },
-    {
-      name: "Why Installing Software Updates Makes Us WannaCry",
-      url:
-        "https://www.scientificamerican.com/article/why-installing-software-updates-makes-us-wannacry/"
-    },
-    {
-      name:
-        "Everything You Ever Wanted to Know About Microsoft Windows Updates",
-      url:
-        "https://www.automox.com/blog/everything-you-want-microsoft-windows-updates"
-    },
-    {
-      name: "Patching Is Failing as a Security Paradigm",
-      url:
-        "https://motherboard.vice.com/en_us/article/439wbw/patching-is-failing-as-a-security-paradigm"
-    }
+      name: "همیشه بروزرسانی کنید",
+      url: "https://hamed.blog/update/"
+    }    
   ]
 };
