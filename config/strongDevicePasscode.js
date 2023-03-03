b83227100ebc2a8440046f0c22f8843bafa6d62e
Hamed <1385656+Hameds@users.noreply.github.com>
2023-03-03 10:46:08 +0330

    change passcode resource titles
    Signed-off-by: Hamed <1385656+Hameds@users.noreply.github.com>

--- config/strongDevicePasscode.js
+++ config/strongDevicePasscode.js
@@ -7,22 +7,13 @@ export default {
   اثر انگشت (TouchID) و تشخیص چهره (FaceID) را موقع مسافرت خاموش کنید.
   
   \n\nباید روال سخت‌گیرانه‌ای برای قفل شدن دستگاه‌هایتان اعمال کنید. همیشه برای باز شدن قفل به یک کد عبور نیاز باشد و اطمینان حاصل کنید که یک دستگاه بیش از یکی دو دقیقه بدون مراقبت رها نشود.`,
-  resources: [
+  resources: [    
     {
-      name: "How long it takes to break a passcode",
-      url: "http://fortune.com/2016/03/18/apple-fbi-iphone-passcode-hack/"
-    },
-    {
-      name: "How to temporarily disable TouchID or FaceID",
-      url:
-        "https://www.iphonelife.com/content/how-to-temporarily-disable-touch-id-or-face-id-your-iphone"
-    },
-    {
-      name: "Changing your iOS passcode",
+      name: "تغییر PassCode در iOS",
       url: "https://support.apple.com/en-us/HT204060"
     },
     {
-      name: "Changing your Android passcode",
+      name: "تغییر PassCode در Android",
       url: "https://phandroid.com/2014/03/20/android-101-lock-screen/"
     }
   ]
