c9451637c275e6d4ac7f2afdd13bdabd83aee77b
Hamed <1385656+Hameds@users.noreply.github.com>
2023-03-03 10:50:19 +0330

    update encrypt hardware resources
    Signed-off-by: Hamed <1385656+Hameds@users.noreply.github.com>

--- config/encryptedHardware.js
+++ config/encryptedHardware.js
@@ -9,32 +9,12 @@ export default {
 `,
   resources: [
     {
-      name: "Why you should encrypt your computer",
-      url: "https://theintercept.com/2015/04/27/encrypting-laptop-like-mean/"
-    },
+      name: "راهنمای سریع رمزنگاری سیستم عامل‌های موبایل و دسکتاپ",
+      url: "https://hamed.blog/encryption/"
+    },   
     {
-      name:
-        "Why you should be encrypting your devices, and how to easily do it",
-      url:
-        "https://gizmodo.com/why-you-should-be-encrypting-your-devices-and-how-to-ea-1798698901"
-    },
-    {
-      name: "Encrypt your iOS backups",
+      name: "رمزنگاری نسخه پشتیبان iOS (backup) در iOS",
       url: "https://support.apple.com/en-us/HT205220"
-    },
-    {
-      name: "Encrypt your Android devices",
-      url:
-        "https://www.howtogeek.com/141953/how-to-encrypt-your-android-phone-and-why-you-might-want-to/"
-    },
-    {
-      name: "Enable full-disk encryption on Windows PCs",
-      url:
-        "https://www.howtogeek.com/234826/how-to-enable-full-disk-encryption-on-windows-10/"
-    },
-    {
-      name: "Encrypt your Mac",
-      url: "https://support.apple.com/en-us/HT204837"
-    }
+    }   
   ]
 };
